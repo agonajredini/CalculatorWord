4da6a133cb9f13a4c0a82661db138c71afd674db
Agon Ajredini <48863247+agonajredini@users.noreply.github.com>
2024-01-03 15:03:18 +0100

    Update calcmore.js

--- calcmore.js
+++ calcmore.js
@@ -3,7 +3,7 @@ var fs = require("fs");
 var fjalor = fs.readFileSync("fjalori.txt").toString();
 
 fjalor = fjalor.split("\r\n");
-var badletters = /[acdfjkmnpqrtuvxyzACDFJKMNPQRTUVXYZëç-]/;
+var badletters = /[acdfjkmnpqrtuvxyACDFJKMNPQRTUVXYëç-]/;
 var longestWord = [""];
 
 for (var newword of fjalor) {
@@ -17,7 +17,7 @@ for (var newword of fjalor) {
     if (newword.length < longestWord[0].length) {
         continue;
     }
-    else if (newword.length == longestWord[0].length) {
+    if (newword.length == longestWord[0].length) {
         longestWord.push(newword);
     }
     else
