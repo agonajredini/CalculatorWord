2f530ab94335537cec62bc4753b85025fb237c59
Agon Ajredini <48863247+agonajredini@users.noreply.github.com>
2024-01-03 14:21:02 +0100

    Added the fixed code and new dictionary
    New dictionary containing more than 700,000 words

---
+++ calcmore.js
@@ -0,0 +1,32 @@
+var fs = require("fs");
+
+var fjalor = fs.readFileSync("fjalori.txt").toString();
+
+fjalor = fjalor.split("\r\n");
+var badletters = /[acdfjkmnpqrtuvxyzACDFJKMNPQRTUVXYZëç-]/;
+var longestWord = [""];
+
+for (var newword of fjalor) {
+    
+    if (newword.match(badletters)) {
+        continue;
+    }
+    if (newword.length > 10) {
+        continue;
+    }
+    if (newword.length < longestWord[0].length) {
+        continue;
+    }
+    else if (newword.length == longestWord[0].length) {
+        longestWord.push(newword);
+    }
+    else
+    {
+        longestWord = [newword];
+    }
+}
+
+console.log(longestWord);
+
+
+
